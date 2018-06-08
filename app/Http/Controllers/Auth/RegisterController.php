ae05ee7c15b4a04517dc04ffe1c9e0ba1e2632cd
kosuke.yuasa <kosuke.yuasa@rakuten.com>
2018-06-08 03:53:37 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,6 +22,8 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    
+
 
     /**
      * Where to redirect users after registration.
@@ -66,7 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' =>  bcrypt($data['password']),
         ]);
     }
 }
